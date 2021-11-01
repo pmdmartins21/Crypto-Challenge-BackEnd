1088d43d78cbbea6aa43d9438d7dfa3f09c560fe
Pedro Martins <pmdmartins@gmail.com>
2021-11-01 18:46:09 +0000

    api created and a few initial tests

---
+++ API/test/jest.test.js
@@ -0,0 +1,10 @@
+test('Validar as principais operações do JEST', () => {
+  let number = null;
+  expect(number).toBeNull();
+  number = 10;
+  expect(number).not.toBeNull();
+  expect(number).toBe(10);
+  expect(number).toEqual(10);
+  expect(number).toBeGreaterThan(9);
+  expect(number).toBeLessThan(11);
+});
