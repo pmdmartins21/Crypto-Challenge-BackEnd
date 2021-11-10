8d4358ce8b4e4e86fd0e34440fdb253f54843a03
Pedro Martins <pmdmartins@gmail.com>
2021-11-10 22:10:52 +0000

    created users routing

--- API/test/routes/users.test.js
+++ API/test/routes/users.test.js
@@ -6,7 +6,7 @@ test('Test #1 - Listar os utilizadores', () => {
   return request(app).get('/users')
     .then((res) => {
       expect(res.status).toBe(200);
-      expect(res.body).toHaveLength(1);
+      expect(res.body).toHaveLength(2);
       expect(res.body[0]).toHaveProperty('firstName', 'Pedro');
     });
 });
