62c132102547ea65ba7df59a3601970cec8c0eda
Pedro Martins <pmdmartins@gmail.com>
2021-12-06 18:14:53 +0000

    final tests

--- API/src/config/router.js
+++ API/src/config/router.js
@@ -10,6 +10,7 @@ module.exports = (app) => {
   secureRouter.use('/games', app.routes.games);
   secureRouter.use('/leaderboards', app.routes.leaderboards);
   secureRouter.use('/gameWallet', app.routes.gameWallet);
+  secureRouter.use('/gameUsers', app.routes.gameWallet);
   secureRouter.use('/transactions', app.routes.transactions);
 
   app.use('/v1', app.config.passport.authenticate(), secureRouter);
--- API/test/routes/leaderboards.test.js
+++ API/test/routes/leaderboards.test.js
@@ -5,7 +5,7 @@ const app = require('../../src/app');
 
 const username = `${Date.now()}`;
 const secret = 'CdTp!DWM@202122';
-const MAIN_ROUTE = '/v1/users';
+const MAIN_ROUTE = '/v1/leaderboards';
 
 let user;
 
@@ -17,5 +17,18 @@ beforeAll(async () => {
 });
 
 
-test('Teste #13 - Obter o leaderboard de um determinado jogo', () => {});
-
+test('Teste #13 - Obter o leaderboard de um determinado jogo', () => {
+  return app.db('games_users').insert(
+    {users_id: user.id, game_id: testGame.id, cash: 20000 }, ['id'],
+    )
+    .then((game_user) => app.db('leaderboards').insert(
+      {game_user_id: game_user.id , finalCashBalance: game_user.cash }
+    )
+    .then(() => request(app).get(`${MAIN_ROUTE}/${testGame.id}`)
+    .set('authorization', `bearer ${user.token}`)
+    .then((res) => {
+      expect(res.status).toBe(200);
+      expect(res.body[0].game_user_id).toBe(game_user.id);
+      expect(res.body[0].finalCashBalance).toBe(20000);
+    })));
+});
