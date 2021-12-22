93ea0333da21258ad50b9e930e1797563893eaad
Pedro Martins <pmdmartins@gmail.com>
2021-12-22 00:09:07 +0000

    changes routes and services

---
+++ API/src/services/gameUser.js
@@ -0,0 +1,43 @@
+const ValidationError = require('../errors/validationError');
+
+module.exports = (app) => {
+  const findAll = (filter = {}) => {
+    return app.db('games_users').where(filter).select('*');
+  };
+
+  const findOne = (filter = {}) => {
+    return app.db('games_users').where(filter).first();
+  };
+
+  // const save = async (game) => {
+  //   if (!game.startDate) throw new ValidationError('StartDate é um atributo obrigatório');
+
+  //   // const gameDb = await findOne({ name: crypto.name });
+  //   // if (cryptoDb) throw new ValidationError('Name duplicado na Bd');
+
+  //   return app.db('games').insert(game, ['id', 'startDate']);
+  // };
+
+  const update = async (gameUserId, cryptoCost) => {
+    const gameUserDb = await findOne({ id: gameUserId });
+    let userBalance = Number(gameUserDb.cashBalance);
+    if(userBalance < cryptoCost.cost) throw new ValidationError('Não tem saldo suficiente para a transação');
+    
+    let newBalance = gameUserDb.cashBalance - cryptoCost.cost;
+
+    return app.db('games_users')
+      .where({ id: gameUserId })
+      .update({cashBalance: newBalance}, '*');
+  };
+
+  // const remove = async (id) => {
+  //   const game = await app.services.game.findOne({ id: id });
+  //   if (!game) throw new ValidationError('O Game não existe na BD');
+
+  //   return app.db('games')
+  //     .where({ id })
+  //     .del();
+  // };
+
+  return { findAll, findOne, update };
+};
