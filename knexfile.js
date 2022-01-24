2d147c1d917db2a352b74d5a5899f7a45f7fb334
Pedro Martins <pmdmartins@gmail.com>
2022-01-24 23:13:59 +0000

    logs and node_env vars

--- knexfile.js
+++ knexfile.js
@@ -11,6 +11,27 @@ module.exports = {
     migrations: {
       directory: 'src/migrations',
     },
+    seeds: {
+      directory: './seeds'
+    },
+    pool: {
+      min: 0,
+      max: 50,
+      propagateCreateError: false,
+    },
+  },
+  prod: {
+    client: 'pg',
+    connection: {
+      host: 'localhost',
+      user: 'postgres',
+      password: 'ROOT',
+      database: 'cryptochallengeprod',
+    },
+    debug: false,
+    migrations: {
+      directory: 'src/migrations'
+    },
     pool: {
       min: 0,
       max: 50,
--- src/app.js
+++ src/app.js
@@ -1,20 +1,26 @@
 const app = require('express')();
 const cors = require('cors');
 const consign = require('consign');
-
+const winston = require('winston');
+const uuid = require('uuidv4');
 const knex = require('knex');
+
 const knexfile = require('../knexfile');
 
-app.db = knex(knexfile.test);
+app.db = knex(knexfile[process.env.NODE_ENV]);
 app.use(cors());
 
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*"); // update to match 
-//   //the domain you will make the request from
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-//   });
-
+app.log = winston.createLogger({
+  level: 'debug',
+  transports: [
+    new winston.transports.Console({ format: winston.format.json({ space: 1 }) }),
+    new winston.transports.File({
+      filename: 'log/error.log',
+      level: 'warn',
+      format: winston.format.combine(winston.format.timestamp(), winston.format.json({ space: 1 })),
+    }),
+  ],
+});
 
 consign({ cwd: 'src', verbose: false })
   .include('./config/passport.js')
@@ -25,6 +31,7 @@ consign({ cwd: 'src', verbose: false })
   .into(app);
 
 app.get('/', (req, res) => {
+  app.log.debug('passei por aqui');
   res.status(200).send();
 });
 
@@ -33,7 +40,9 @@ app.use((err, req, res, next) => {
   if (name === 'validationError') res.status(400).json({ error: message });
   else if (name === 'forbiddenError') res.status(403).json({ error: message });
   else {
-    res.status(500).json({ name, message, stack }); //testar
+    const id = uuid();
+    app.log.error(name, message, stack);
+    res.status(500).json({ id, error: 'Erro de sistema' }); 
   }
   next(err);
 });
